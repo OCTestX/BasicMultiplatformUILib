ceea1dda591e990bb5652656b3706c523e36b865
octest <octest8848@qq.com>
2025-05-03 11:21:57 +0800

    feat(library): 更新语言枚举并增加对应文本
    - 在 Language枚举中添加 text 属性，为每种语言增加对应的文本
    - 更新项目版本号至 0.1.4.1

--- library/src/commonMain/kotlin/ui/global/Language.kt
+++ library/src/commonMain/kotlin/ui/global/Language.kt
@@ -5,8 +5,11 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 // 定义语言枚举
-enum class Language {
-    English, ChineseSimplified, ChineseTraditional, Japanese,
+enum class Language(val text: String) {
+    English("\uD83C\uDF10 International English"), ChineseSimplified("\uD83C\uDDE8\uD83C\uDDF3 简体中文"), ChineseTraditional(
+        "\uD83C\uDC04 繁體中文"
+    ),
+    Japanese("\uD83C\uDDEF\uD83C\uDDF5 日本語"),
 }
 
 // 创建 CompositionLocal，默认值为英语
